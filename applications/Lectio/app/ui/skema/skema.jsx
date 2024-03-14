4010fa5af237dbd3dbb06083f66f4ad54e836339
Jeppe <jepbe123@gmail.com>
2024-03-14 22:48:21 +0100

    aftenes arbejde

--- applications/Lectio/app/ui/skema/skema.jsx
+++ applications/Lectio/app/ui/skema/skema.jsx
@@ -1,6 +1,61 @@
 import SkemaModule from "./skemaModule";
 
-// Lav så man kan skrive skema ind og ikke skal lede i filen efter enkelte dage.
+
+// Monday
+const scheduleMon = [
+    { subject: 'MA', teacher: 'jacs', room: 'SLWI-4110', key: 1, visibility: 'show' },
+    { subject: 'MA', teacher: 'jacs', room: 'SLWI-4110', key: 2, visibility: 'show' },
+    { subject: 'Fy', teacher: 'hnpo', room: 'SLWI-4110', key: 3, visibility: 'show' },
+    { subject: 'Fy', teacher: 'hnpo', room: 'SLWI-4110', key: 4, visibility: 'show' },
+    { subject: 'sa', teacher: 'mmje', room: 'SLWI-4110', key: 5, visibility: 'show' },
+    { subject: 'So', teacher: 'kibo', room: 'SLWI-4110', key: 6, visibility: 'show' },
+    { subject: '', teacher: '', room: '', key: 7, visibility: 'hide' },
+];
+
+// Tuesday
+const scheduleTue = [
+    { subject: 'En2', teacher: 'mgja', room: 'SLWI-4110', key: 1, visibility: 'show' },
+    { subject: 'En2', teacher: 'mgja', room: 'SLWI-4110', key: 2, visibility: 'show' },
+    { subject: 'bi', teacher: 'bhjo', room: 'SLWI-4110', key: 3, visibility: 'show' },
+    { subject: 'bi', teacher: 'bhjo', room: 'SLWI-4110', key: 4, visibility: 'show' },
+    { subject: 'sa', teacher: 'mmje', room: 'SLWI-4110', key: 5, visibility: 'show' },
+    { subject: '', teacher: '', room: '', key: 6, visibility: 'hide' },
+    { subject: '', teacher: '', room: '', key: 7, visibility: 'hide' },
+];
+
+// Wednesday
+const scheduleWed = [
+    { subject: 'Ke', teacher: 'pgra', room: 'SLWI-4110', key: 1, visibility: 'show' },
+    { subject: 'Ke', teacher: 'pgra', room: 'SLWI-4110', key: 2, visibility: 'show' },
+    { subject: 'MA', teacher: 'jacs', room: 'SLWI-4110', key: 3, visibility: 'show' },
+    { subject: 'MA', teacher: 'jacs', room: 'SLWI-4110', key: 4, visibility: 'show' },
+    { subject: 'Ti', teacher: 'hnpo', room: 'SLWI-4110', key: 5, visibility: 'show' },
+    { subject: 'Ti', teacher: 'hnpo', room: 'SLWI-4110', key: 6, visibility: 'show' },
+    { subject: '', teacher: '', room: '', key: 7, visibility: 'hide' },
+];
+
+// Thursday
+const scheduleThu = [
+    { subject: 'Ti', teacher: 'hnpo', room: 'SLWI-4110', key: 1, visibility: 'show' },
+    { subject: 'Ti', teacher: 'hnpo', room: 'SLWI-4110', key: 2, visibility: 'show' },
+    { subject: 'DA', teacher: 'kibo', room: 'SLWI-4110', key: 3, visibility: 'show' },
+    { subject: 'DA', teacher: 'kibo', room: 'SLWI-4110', key: 4, visibility: 'show' },
+    { subject: '', teacher: '', room: '', key: 5, visibility: 'hide' },
+    { subject: '', teacher: '', room: '', key: 6, visibility: 'hide' },
+    { subject: '', teacher: '', room: '', key: 7, visibility: 'hide' },
+];
+
+// Friday
+const scheduleFri = [
+    { subject: '', teacher: '', room: '', key: 1, visibility: 'hide' },
+    { subject: 'En2', teacher: 'mgja', room: 'SLWI-4110', key: 2, visibility: 'show' },
+    { subject: 'kit', teacher: 'rety', room: 'SLWI-4110', key: 3, visibility: 'show' },
+    { subject: 'kit', teacher: 'rety', room: 'SLWI-4110', key: 4, visibility: 'show' },
+    { subject: 'Fy', teacher: 'hnpo', room: 'SLWI-4110', key: 5, visibility: 'show' },
+    { subject: '', teacher: '', room: '', key: 6, visibility: 'hide' },
+    { subject: '', teacher: '', room: '', key: 7, visibility: 'hide' },
+];
+
 
 export default function skema() {
     return (
@@ -20,117 +75,74 @@ export default function skema() {
                         </thead>
                         <tbody>
                             
+                            {/* Module 1 */}
                             <tr>
-                             <td>  <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">1.</span> </td>
-                                <td>
-                                    <SkemaModule subject="MA" teacher="jacs" room="SLWI-4110"/> 
-                                </td>
-                                
-                                <td><SkemaModule subject="DA" teacher="kibo" room="SLWI-4110" /></td>   
-                                <td>    
-                                    <SkemaModule subject="MA" teacher="jacs" room="SLWI-4110" / >
-                                </td>
-                                <td>d</td>
-                                <td>e</td>
+                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">1.</span> </td>
+                                <td><SkemaModule subject={scheduleMon[0].subject} teacher={scheduleMon[0].teacher} room={scheduleMon[0].room} visibility={scheduleMon[0].visibility} /></td>
+                                <td><SkemaModule subject={scheduleTue[0].subject} teacher={scheduleTue[0].teacher} room={scheduleMon[0].room} visibility={scheduleTue[0].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[0].subject} teacher={scheduleWed[0].teacher} room={scheduleWed[0].room} visibility={scheduleWed[0].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[0].subject} teacher={scheduleThu[0].teacher} room={scheduleThu[0].room} visibility={scheduleThu[0].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[0].subject} teacher={scheduleFri[0].teacher} room={scheduleFri[0].room} visibility={scheduleFri[0].visibility} /></td>
                             </tr>
 
-                            {/* Tue */}
-                            <tr>
+                            {/* Module 2 */}
+                            <tr className="m-2">
                                 <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">2.</span> </td>
-                                    <td>
-                                        <SkemaModule subject="Ti" teacher="hnpo" room="SLWI-4110" />
-                                    </td>
-
-                                <td>                                    <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
-                                        <div className="flex flex-col text-[#0D3F70]">
-                                            <span className="font-bold text-xl">S 1x MA</span>
-                                            <span>jacs</span>
-                                            <span>SLWI-4110</span>
-                                        </div>
-                                    </div></td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[1].subject} teacher={scheduleMon[1].teacher} room={scheduleMon[1].teacher} visibility={scheduleMon[1]} /></td>
+                                <td><SkemaModule subject={scheduleTue[1].subject} teacher={scheduleTue[1].teacher} room={scheduleTue[1].room} visibility={scheduleTue[1].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[1].subject} teacher={scheduleWed[1].teacher} room={scheduleWed[1].room} visibility={scheduleWed[1].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[1].subject} teacher={scheduleThu[1].teacher} room={scheduleThu[1].room} visibility={scheduleThu[1].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[1].subject} teacher={scheduleFri[1].teacher} room={scheduleFri[1].room} visibility={scheduleFri[1].visibility} /></td>
                             </tr>
-
                             
+                            {/* Module 3 */}
                             <tr>
                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">3.</span> </td>
-                                <td>
-                                    <SkemaModule subject="Ti" teacher="hnpo" room="SLWI-4110" />
-                                </td>
-                                <td>b</td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[2].subject} teacher={scheduleMon[2].teacher} room={scheduleMon[2].room} visibility={scheduleMon[2]} /></td>
+                                <td><SkemaModule subject={scheduleTue[2].subject} teacher={scheduleTue[2].teacher} room={scheduleTue[2].room} visibility={scheduleTue[2].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[2].subject} teacher={scheduleWed[2].teacher} room={scheduleWed[2].room} visibility={scheduleWed[2].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[2].subject} teacher={scheduleThu[2].teacher} room={scheduleThu[2].room} visibility={scheduleThu[2].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[2].subject} teacher={scheduleFri[2].teacher} room={scheduleFri[2].room} visibility={scheduleFri[2].visibility} /></td>
                             </tr>
-
-                            {/* Thu */}
+                            
+                            {/* Module 4 */}
                             <tr>
                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">4.</span> </td>
-                                <td>
-                                    <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
-                                        <div className="flex flex-col text-[#0D3F70]">
-                                            <span className="font-bold text-xl">S 1x MA</span>
-                                            <span>jacs</span>
-                                            <span>SLWI-4110</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>b</td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[3].subject} teacher={scheduleMon[3].teacher} room={scheduleMon[3].room} visibility={scheduleMon[3].visibility} /></td>
+                                <td><SkemaModule subject={scheduleTue[3].subject} teacher={scheduleTue[3].teacher} room={scheduleTue[3].room} visibility={scheduleTue[3].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[3].subject} teacher={scheduleWed[3].teacher} room={scheduleWed[3].room} visibility={scheduleWed[3].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[3].subject} teacher={scheduleThu[3].teacher} room={scheduleThu[3].room} visibility={scheduleThu[3].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[3].subject} teacher={scheduleFri[3].teacher} room={scheduleFri[3].room} visibility={scheduleFri[3].visibility} /></td>
                             </tr>
-
-                            {/* Fri */}
+                            
+                            {/* Module 5 */}
                             <tr>
                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">5.</span> </td>
-                                <td>
-                                    <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
-                                        <div className="flex flex-col text-[#0D3F70]">
-                                            <span className="font-bold text-xl">S 1x MA</span>
-                                            <span>jacs</span>
-                                            <span>SLWI-4110</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>b</td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[4].subject} teacher={scheduleMon[4].teacher} room={scheduleMon[4].room} visibility={scheduleMon[3]} /></td>
+                                <td><SkemaModule subject={scheduleTue[4].subject} teacher={scheduleTue[4].teacher} room={scheduleTue[4].room} visibility={scheduleTue[4].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[4].subject} teacher={scheduleWed[4].teacher} room={scheduleWed[4].room} visibility={scheduleWed[4].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[4].subject} teacher={scheduleThu[4].teacher} room={scheduleThu[4].room} visibility={scheduleThu[4].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[4].subject} teacher={scheduleFri[4].teacher} room={scheduleFri[4].room} visibility={scheduleFri[4].visibility} /></td>
                             </tr>
+                            
+                            {/* Module 6 */}
                             <tr>
                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">6.</span> </td>
-                            <td>
-                                    <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
-                                        <div className="flex flex-col text-[#0D3F70]">
-                                            <span className="font-bold text-xl">S 1x MA</span>
-                                            <span>jacs</span>
-                                            <span>SLWI-4110</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>b</td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[5].subject} teacher={scheduleMon[5].teacher} room={scheduleMon[5].room} visibility={scheduleMon[5].visibility} /></td>
+                                <td><SkemaModule subject={scheduleTue[5].subject} teacher={scheduleTue[5].teacher} room={scheduleTue[5].room} visibility={scheduleTue[5].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[5].subject} teacher={scheduleWed[5].teacher} room={scheduleWed[5].room} visibility={scheduleWed[5].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[5].subject} teacher={scheduleThu[5].teacher} room={scheduleThu[5].room} visibility={scheduleThu[5].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[5].subject} teacher={scheduleFri[5].teacher} room={scheduleFri[5].room} visibility={scheduleFri[5].visibility} /></td>
                             </tr>
+
+                            {/* Module 7 */}
                             <tr>
                             <td> <span className="h-[50px] bg-slate-300 py-10 px-2 rounded-e-md text-slate-600 font-bold">7.</span> </td>
-                            <td>
-                                    <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
-                                        <div className="flex flex-col text-[#0D3F70]">
-                                            <span className="font-bold text-xl">S 1x MA</span>
-                                            <span>jacs</span>
-                                            <span>SLWI-4110</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>b</td>
-                                <td>c</td>
-                                <td>d</td>
-                                <td>e</td>
+                                <td><SkemaModule subject={scheduleMon[6].subject} teacher={scheduleMon[6].teacher} room={scheduleMon[6].room} visibility={scheduleMon[6].visibility} /></td>
+                                <td><SkemaModule subject={scheduleTue[6].subject} teacher={scheduleTue[6].teacher} room={scheduleTue[6].room} visibility={scheduleTue[6].visibility} /></td>
+                                <td><SkemaModule subject={scheduleWed[6].subject} teacher={scheduleWed[6].teacher} room={scheduleWed[6].room} visibility={scheduleWed[6].visibility} /></td>
+                                <td><SkemaModule subject={scheduleThu[6].subject} teacher={scheduleThu[6].teacher} room={scheduleThu[6].room} visibility={scheduleThu[6].visibility} /></td>
+                                <td><SkemaModule subject={scheduleFri[6].subject} teacher={scheduleFri[6].teacher} room={scheduleFri[6].room} visibility={scheduleFri[6].visibility} /></td>
                             </tr>
                         </tbody>
                     </table>
--- applications/Lectio/app/ui/skema/skemaModule.jsx
+++ applications/Lectio/app/ui/skema/skemaModule.jsx
@@ -1,12 +1,12 @@
 export default function skemaModule( args ) {
     
-    if (args.hide) {
+    if (args.visibility == 'hide') {
         return (
-            null
+            <div className="min-h-[88px]"></div>
         );
     } else {
         return (
-        <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg">
+        <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg max-w-[275px] max-h-[88px] flex items-center">
             <div className="flex flex-col text-[#0D3F70]">
                 <span className="font-bold text-xl">S 1x { args.subject }</span>
                 <span>{ args.teacher }</span>
@@ -15,6 +15,4 @@ export default function skemaModule( args ) {
         </div> 
         );
     }
-
-
 }
