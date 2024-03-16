4a63543b4e077b636d54f5fcd044820705421ad8
Jeppe Bøgeskov <jepbe123@gmail.com>
2024-03-17 00:35:26 +0100

    Nice coding session. I have implemented a new iccon system, that shows if each module has homework and or a note from the teacher. Also som genneral improvements, design e.g.

--- applications/Lectio/app/ui/skema/skemaModule.jsx
+++ applications/Lectio/app/ui/skema/skemaModule.jsx
@@ -1,16 +1,52 @@
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+
+
+
 export default function skemaModule( args ) {
-    
-    if (args.visibility == 'hide') {
+
+    if ( args.visibility == 'hide' ) {
         return (
             <div className="min-h-[88px]"></div>
         );
     } else {
         return (
-        <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg max-w-[275px] max-h-[88px] flex items-center">
-            <div className="flex flex-col text-[#0D3F70]">
+        <div className="bg-[#9CCEFF] border-l-[10px] border-l-[#1E90FF] p-4 rounded-lg max-w-[275px] max-h-[88px] flex justify-between cursor-pointer hover:opacity-80">
+            <div className="flex flex-col text-[#0D3F70] justify-center">
                 <span className="font-bold text-xl">S 1x { args.subject }</span>
                 <span>{ args.teacher }</span>
                 <span>{ args.room }</span>
+            </div>
+            
+            <div className="text-[#0D3F70] flex flex-row float-right">
+                <div>
+                    { (() => {
+                        if ( args.note != '' ) {
+                            return (
+                                <span><ChatBubbleOvalLeftIcon className="h-5"/></span>
+                                );
+                            } else {
+                                return (
+                                    <span></span>
+                                    );
+                                }
+                                
+                            })()}
+                </div>
+                <div>
+                    { (() => {
+                        if ( args.homework != '' ) {
+                            return (
+                                <span><PencilSquareIcon className="h-5"/></span>
+                                );
+                            } else {
+                                return (
+                                    <span></span>
+                            );
+                        }
+                        
+                    })()}
+                </div>
             </div>
         </div> 
         );
